e705dd2b0f6b776cf3ff72a977e335af08b61048
Navya0099 <navyapathipaka526@gmail.com>
2021-12-14 14:21:51 +0530

    firstcommit

---
+++ app.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, app, render_template, request,url_for, redirect
+import pickle
+import pandas as pd
+import numpy as np
+
+
+app = Flask(__name__)
+
+forecast = pd.read_pickle('goldforecast.pkl')
+forecast.head()
+
+@app.route('/')
+
+def home():
+    return render_template('home.html', tables= [forecast.to_html(classes = 'forecast')],
+    titles = ['Date','Average forecast for the month'], )
+
+@app.route('/Name', methods = ['POST'])
+def Name():
+    if request.method == 'POST':
+        return render_template('Name.html')
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
